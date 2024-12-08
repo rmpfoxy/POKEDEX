1deb95b36df4bb91adc4d12d0f207c3e65428cec
rmpfoxy <rodrigomp8@gmail.com>
2024-12-08 16:26:16 -0300

    Primeiro commit do meu POKEDEX

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 10
+let offset = 0;
+
+const maxRecords = 151
+
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number"> #${pokemon.number} </span>
+                <span class="name"> ${pokemon.name} </span>
+    
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}"> ${type} </li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+// Os argumentos estão invertidos, eu acho ==> Corrigido! //
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordWithNextPage = offset + limit
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
